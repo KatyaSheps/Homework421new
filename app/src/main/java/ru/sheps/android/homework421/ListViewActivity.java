336348f2fbf3481c7a872d488e5461ea8d968487
Katya Sheps <katya@sheps.ru>
2019-06-23 22:14:07 +0300

    Add new image

--- app/src/main/java/ru/sheps/android/homework421/ListViewActivity.java
+++ app/src/main/java/ru/sheps/android/homework421/ListViewActivity.java
@@ -3,12 +3,14 @@ package ru.sheps.android.homework421;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ListViewActivity extends AppCompatActivity {
 
-    private List<Sample> values;
+    private List<Sample> values = new ArrayList<>();
 
     private MyAdapter adapter;
 
@@ -19,11 +21,11 @@ public class ListViewActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.list);
 
-        values.add(new Sample("Название сэмпла", "Название урока", R.drawable.ic_ondemand_video_black_24dp));
-        values.add(new Sample("Название сэмпла", "Название урока", R.drawable.ic_ondemand_video_black_24dp));
-        values.add(new Sample("Название сэмпла", "Название урока", R.drawable.ic_ondemand_video_black_24dp));
-        values.add(new Sample("Название сэмпла", "Название урока", R.drawable.ic_ondemand_video_black_24dp));
-        values.add(new Sample("Название сэмпла", "Название урока", R.drawable.ic_ondemand_video_black_24dp));
+        values.add(new Sample("Название семпла", "Название урока", R.drawable.ic_ondemand_video_black_24dp));
+        values.add(new Sample("Введите адрес", "Домашнее задание 2.1.2", R.drawable.adress));
+        values.add(new Sample("Календарь задач", "Домашнее задание 2.1.3", R.drawable.calendar));
+        values.add(new Sample("Оплатить", "Домашнее задание 2.1.1", R.drawable.pay));
+        values.add(new Sample("Сохранить текст", "Домашнее задание 2.2.1", R.drawable.savetext));
 
         adapter = new MyAdapter(this, R.layout.my_simple_list_item, values);
         listView.setAdapter(adapter);
